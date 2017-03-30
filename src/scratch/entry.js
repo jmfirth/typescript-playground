c8283fbfa890cf81e4cf0d1cc6da4161ed4ed445
Justin Firth <jmfirth@gmail.com>
2017-03-30 11:06:31 -0400

    Added gh-pages-deploy settings; Removed extraneous

--- src/scratch/entry.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var util_1 = require("./util");
-util_1["default"]();
--- src/scratch/util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function sayHello() {
-    console.log('hello!');
-}
-exports["default"] = sayHello;
